fa8c07f6b0b1f48ca0d5eaf46c0f102c724d0d6e
Paulo Romano <pauloromanocarvalho@gmail.com>
2018-12-14 15:58:56 -0200

    Use group to separate commands

--- strings/textwrapper/__main__.py
+++ strings/textwrapper/__main__.py
@@ -3,7 +3,15 @@ import click
 import utils
 
 
-@click.command()
+@click.group()
+def command_group():
+    pass
+
+
+@command_group.command(
+    name='text',
+    short_help='Split text into lines.'
+)
 @click.argument('text')
 @click.option('--max-length', default=40)
 @utils.command_surrounded_by_frame
@@ -13,4 +21,4 @@ def textwrapper(text, max_length):
 
 
 if __name__ == '__main__':
-    textwrapper()
+    command_group()
